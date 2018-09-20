d6ceff7871e26aec1f669407e3e73a6bab791e4d
awasicek <awasicek@Andrews-MacBook-Pro.local>
2018-09-20 09:41:19 -0700

    updated 6 again

--- test6.js
+++ test6.js
@@ -1,3 +1,4 @@
 console.log("昨夜のコンdroit à la  des intérêtsDas Brot ist nicht schön. Ich kaufe Brot in der Bäckerei.");
 const oops = "test updated";
 const why = "should be here";
+const hello = "there";
