d8ce6f6dbe8e2de0fc97c4076708678b36772f3f
Andrew Wasicek <awasicek@Andrews-MacBook-Pro.local>
2018-10-01 17:20:35 -0700

    updated tealium

---
+++ Tealium.js
@@ -0,0 +1,3 @@
+console.log("The Tealium Universal Data Hub is the industry's most comprehensive and flexible \n" +
+    "approach to collect and deliver customer data. Manage tags, APIs, and integrate with over \n" +
+    "1,000 vendors.");
--- test5-multiple_foreign_chars.js
+++ test5-multiple_foreign_chars.js
@@ -1,4 +1,4 @@
-console.log("The Tealium Universal Data Hub is the industry's most comprehensive and flexible \n" +
-    "approach to collect and deliver customer data. Manage tags, APIs, and integrate with over \n" +
-    "1,000 vendors.");
-
+var test5;
+console.log("昨夜のコンdroit à la protection des intérêtsDas Brot ist nicht schön. Ich kaufe Brot in der Bäckerei.");
+var x = "changed";
+const mod = "modified";
