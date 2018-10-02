c9d15900e270a2a8a32a7e7dacf04db5f32aeec5
Andrew Wasicek <awasicek@Andrews-MacBook-Pro.local>
2018-10-01 17:24:17 -0700

    another update to tealium

--- Tealium.js
+++ Tealium.js
@@ -1,3 +1,11 @@
 console.log("The Tealium Universal Data Hub is the industry's most comprehensive and flexible \n" +
     "approach to collect and deliver customer data. Manage tags, APIs, and integrate with over \n" +
     "1,000 vendors.");
+
+let tealiumCustomers = Number.MAX_SAFE_INTEGER;
+while(true) {
+    tealiumCustomers++;
+}
+
+
+
