3265778ebe433195ff97ae8c5375be6779c11851
Andrew Wasicek <awasicek@gmail.com>
2018-10-31 15:49:24 -0700

    more languages

--- character_testing.js
+++ character_testing.js
@@ -1,1 +1,9 @@
-&#000;&#230;&#234;
+console.log("polish - cześć świecie lubię wiedźmin i inne fajne gry od CD projekt czerwone");
+
+console.log("turkish - merhaba dünya Ben CD projekt kırmızı tarafından cadı ve diğer serin oyunları seviyorum");
+
+console.log("croatian - hello world Sviđa mi se witcher i druge cool igre po CD projektu crvenom");
+
+console.log("hebrew - שלום עולם אני אוהב את המכשף ומשחקים מגניבים אחרים על ידי תקליטור projekt אדום");
+
+console.log("armenian - Բարեւ աշխարհ Ես սիրում եմ կախարդական եւ այլ թույն խաղերը CD projekt կարմիրով");
